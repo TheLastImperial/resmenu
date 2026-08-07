5a54d86ac2371decabec924fd6dab6aec806cd70
TheLastImperial <17633123+TheLastImperial@users.noreply.github.com>
2026-08-07 10:27:32 -0700

    Added Remember me

--- src/resmenu/src/main/java/com/thelastimperial/resmenu/config/SecurityConfig.java
+++ src/resmenu/src/main/java/com/thelastimperial/resmenu/config/SecurityConfig.java
@@ -2,22 +2,26 @@ package com.thelastimperial.resmenu.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationConverter jwtAuthenticationConverter;
+    private final RememberMeServices rememberMeServices;
 
-    public SecurityConfig(JwtAuthenticationConverter jwtAuthenticationConverter){
+    public SecurityConfig(JwtAuthenticationConverter jwtAuthenticationConverter,
+        RememberMeServices rememberMeServices
+    ){
         this.jwtAuthenticationConverter = jwtAuthenticationConverter;
+        this.rememberMeServices = rememberMeServices;
     }
 
     @Bean
@@ -35,6 +39,11 @@ public class SecurityConfig {
         .oauth2ResourceServer(oauth2 -> {
             oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter));
         })
+        .rememberMe(rem -> 
+            rem
+            .rememberMeServices(rememberMeServices)
+            .rememberMeParameter("remember-me")
+        )
         .formLogin(
             login ->
                 login
